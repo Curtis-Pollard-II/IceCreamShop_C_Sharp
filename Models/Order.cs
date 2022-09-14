23496f0d91c53ff0f173f8428e77b4143638d76f
Curtis Pollard <clp.the.second@gmail.com>
2022-09-14 15:59:08 -0600

    first commit Wednesday

--- Models/Order.cs
+++ Models/Order.cs
@@ -5,27 +5,12 @@ namespace IceCreamShop_C_Sharp.Models
 {
     public class Order
     {
-        public Order (string name, double price, int scoops, string toppings, int id)
-        {
-            Name = name;
-            Price = price;
-            Scoops = scoops;
-            Toppings = toppings;
-            Id = id;
-        }
-        public Order ()
-        {
-
-        }
-
-        [Required]
-        [MaxLength(100)]
-
+        public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public int? Scoops { get; set; }
         public string Toppings { get; set; }
-        public int Id { get; set; }
+        public int Cone { get; set; }
 
     }
 }
--- Repositories/OrderRepository.cs
+++ Repositories/OrderRepository.cs
@@ -1,16 +1,30 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
 using IceCreamShop_C_Sharp.Models;
+
 
 namespace IceCreamShop_C_Sharp.Repositories
 {
     public class OrderRepository
     {
+        private readonly IDbConnection _db;
+
+        public OrderRepository(IDbConnection db)
+        {
+            _db = db;
+        }
+
         internal List<Order> GetOrders()
         {
-            throw new NotImplementedException();
+            string sql = @"
+            SELECT * FROM Orders;
+            ";
+            List<Order> orders =_db.Query<Order>(sql).ToList();
+            return orders;
         }
     }
 }
